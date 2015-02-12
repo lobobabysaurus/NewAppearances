ad5e7b93fcda73787265cb52c3f05479b4d90c76
PRS <Phil.R.Simmons@gmail.com>
2015-02-12 08:25:02 -0500

    Added logic to dynamically expand and contract content section sizee.  Updated coloring to be less horrible.  Added hover color to buttons

--- base/static/base/base.js
+++ base/static/base/base.js
@@ -5,7 +5,7 @@
  * @creator PRS
  * @created 2/7/2015
  * @last_modified_by PRS
- * @last_modified_date 2/7/2015
+ * @last_modified_date 2/12/2015
  */
 
 /**
@@ -18,9 +18,18 @@ function getCurrentTime() {
     var year = time.getFullYear();
     var hours = time.getHours();
     //Add padding 0s to minutes and seconds if they are less than ten for formatting reasons
-    var minutes = (time.getMinutes() < 10 ? "0" : "" ) + time.getMinutes();
-    var seconds = (time.getSeconds() < 10 ? "0" : "" ) + time.getSeconds();
+    var minutes = addPaddingZeroIfNecessary(time.getMinutes());
+    var seconds = addPaddingZeroIfNecessary(time.getSeconds());
     document.getElementById("time").innerText = hours + ":" + minutes + ":" +seconds+" "+year;
+
+    /**
+     * If a time is less than 10 add a padding 0 to the front of the time value
+     * @param time Time in minutes/seconds
+     * @returns {string} either the original value or a 0 padded value
+     */
+    function addPaddingZeroIfNecessary(time) {
+        return (time < 10 ? "0" : "") + time
+    }
 }
 /**
  * Update the time every second to simulate a clock
@@ -39,4 +48,28 @@ function runClock(){
  */
 function baseOnLoad(){
     runClock();
+    setContentHeight();
+}
+/**
+ * Get the height of all relatively constant size divs that are top level to the body.  Find the difference of the
+ * window size and this values and assign the content div to be this size
+ *
+ * @returns null
+ */
+function setContentHeight(){
+    var navHeight = getElementHeight("navBarContainer") +
+            getElementHeight("templateFooter") +
+            getElementHeight("templateHeader");
+
+    document.getElementById("templateContent").style.height =
+        (window.innerHeight-navHeight)+"px";
+
+    /**
+     * Get the height of an element
+     * @param id HTML  id of an element
+     * @returns {number} Height of element specified by id
+     */
+    function getElementHeight(id) {
+        return document.getElementById(id).offsetHeight;
+    }
 }
