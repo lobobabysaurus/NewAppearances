28c9b7b0e4a9280e76e1e444fe822ec19e0e4734
gpitm <leclair566@gmail.com>
2015-02-22 18:41:46 -0500

    Updated db, services/template,view,model,fixtures,tests

--- services/views.py
+++ services/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.template import RequestContext, loader
+from django.shortcuts import render
 
 from services.models import Service
 
@@ -8,12 +9,7 @@ Get all services and supply this list to the services/services.html template
 """
 def services(request):
     all_services = Service.objects.order_by('order')
-    template = loader.get_template('services/services.html')
 
-    context = RequestContext(request, {
-        'service_list': all_services,
-    })
-
-    return HttpResponse(template.render(context))
+    return render(request, 'services/services.html', {'service_list': all_services,} )
 
 
