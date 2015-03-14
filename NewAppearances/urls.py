92d11d41845d21b7b0b837dcf950a84d4ba96607
PRS <Phil.R.Simmons@gmail.com>
2015-03-14 16:26:04 -0400

    Added support for Documentation

--- NewAppearances/urls.py
+++ NewAppearances/urls.py
@@ -1,13 +1,15 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
+from django.contrib import admin, admindocs
 
 urlpatterns = patterns('',
     #Forward a url with no resource specified to the home application
     url(r'^$', include('home.urls')),
     #Forward a url with the services/ resource to the services application
     url(r'^services/', include('services.urls')),
-    #Forward a utl with the products/ resource to the products application
+    #Forward a url with the products/ resource to the products application
     url(r'^products/', include('products.urls')),
+    #Forward a url with admin/docs/ to the admin documentation url
+    url(r'^admin/docs/', include('django.contrib.admindocs.urls')),
     #Forward a url with admin/ to the admin site url
     url(r'^admin/', include(admin.site.urls)),
 )
