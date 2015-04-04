bdceaa1995cc1d725fdcd4bd4b112ca9cdf1f67b
prs <Phil.R.Simmons@gmail.com>
2015-04-04 17:42:42 -0400

    Added javascript documentation and tool and made javascript more clean and object based

--- base/static/base/js/base.js
+++ base/static/base/js/base.js
@@ -1,82 +1,100 @@
 /**
- * File to implement all javascript resources that will be available to the base application
+ * Module to implement all javascript resources that will be available to the base application
  * at least for the main template
+ *
+ * @module Base
  */
 
 /**
  * Jquery to call all functions that need to be executed when the page loads
  *
- * @returns null
  */
 $(document).ready( function () {
+    var base = new Base();
     //Run page clock
-    setCurrentTime();
-    setInterval(setCurrentTime,1000);
+    base.setCurrentTime();
+    setInterval(function () {base.setCurrentTime();}, 1000);
     //Set content section height
-    setContentHeight();
+    base.setContentHeight();
     //Associate Nav bar click reactions
-    setButtonLinks();
+    base.setButtonLinks();
     //Set the width of the nav buttons
-    setButtonWidth();
+    base.setButtonWidth();
 });
 
 /**
  * Reset the size of the content section and nav buttons if the window is resized
  *
- * @returns null
  */
 $(window).resize( function () {
-    setContentHeight();
-    setButtonWidth();
+    var base = new Base();
+    base.setContentHeight();
+    base.setButtonWidth();
 });
 
 /**
- * Set the width for the navbar
+ * Class representing the base functionality
+ * @class Base
+ * @constructor
  */
-function setButtonWidth(){
-    //get the nav bar div
-    var navBar = $("div.navBar")
-    //Find number of buttons to display
-    var numImages = navBar.find("button").length;
-    //set the width each button to be the size of the container divided by the number of images
-    navBar.find("button").outerWidth(navBar.innerWidth()/numImages);
-}
-/**
- * Set button click functionality
- */
-function setButtonLinks(){
-    $("div.navBar").find("button").each(function () {
-        $(this).click(function () {
-            window.location.href = "/"+this.id+"/";
-        })
-    });
-}
-/**
- * Gets the current time in the format of "HH:mm:ss DD MMMM YYYY"
- *
- * @returns string formatted as HH:mm:ss DD MMMM YYYY
- */
-function getCurrentTime(){return moment( new Date()).format("HH:mm:ss DD MMMM YYYY")}
+function Base() {
 
-/**
- * Set the current time on the 'time' element
- * @returns null
- */
-function setCurrentTime() {
-    $("#time").text(getCurrentTime());
-}
+    /**
+     * Set the width for the navbar
+     * @method setButtonWidth
+     */
+    this.setButtonWidth = function(){
+        //get the nav bar div
+        var navBar = $("div.navBar")
+        //Find number of buttons to display
+        var numImages = navBar.find("button").length;
+        //set the width each button to be the size of the container divided by the number of images
+        navBar.find("button").outerWidth(navBar.innerWidth()/numImages);
+    };
 
-/**
- * Get the height of all relatively constant size divs that are top level to the body.  Find the difference of the
- * window size and this values and assign the content div to be this size
- *
- * @returns null
- */
-function setContentHeight(){
-    //height of all body elements
-    var navHeight = $("#navBarContainer").outerHeight() +
+    /**
+     * Set button click functionality
+     * @method setButtonLinks
+     */
+    this.setButtonLinks= function(){
+        $("div.navBar").find("button").each(function () {
+            $(this).click(function () {
+                window.location.href = "/"+this.id+"/";
+            })
+        });
+    };
+
+    /**
+     * Gets the current time in the format of "HH:mm:ss DD MMMM YYYY"
+     *
+     * @method getCurrentTime
+     * @return {String} Time formatted as HH:mm:ss DD MMMM YYYY
+     */
+    this.getCurrentTime = function(){
+        return moment(new Date()).format("HH:mm:ss DD MMMM YYYY");
+    };
+
+    /**
+     * Set the current time on the 'time' element
+     *
+     * @method setCurrentTime
+     */
+    this.setCurrentTime = function() {
+        $("#time").text(this.getCurrentTime());
+    };
+
+    /**
+     * Get the height of all relatively constant size divs that are top level to the body.  Find the difference of the
+     * window size and this values and assign the content div to be this size
+     *
+     * @method setContentHeight
+     */
+    this.setContentHeight = function(){
+        //height of all body elements
+        var navHeight = $("#navBarContainer").outerHeight() +
             $("#templateFooter").outerHeight() +
             $("#templateHeader").outerHeight();
-    //set the content element to be the difference between the window size and all other body elements
-    $("#templateContent").height(window.innerHeight - navHeight);
+        //set the content element to be the difference between the window size and all other body elements
+        $("#templateContent").height(window.innerHeight - navHeight);
+    };
 }
