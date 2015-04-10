fdc503c7996d14dcf69d1566318fc5c5d9600f53
PRS <Phil.R.Simmons@gmail.com>
2015-04-10 17:16:29 -0400

    Add trivial locaiton finding case from my house to salon

--- location/static/location/js/location.js
+++ location/static/location/js/location.js
@@ -3,15 +3,47 @@
  * @module Location
  */
 
-function initializeMaps () {
-    var mapOptions = {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 8
-    };
-    var map = new google.maps.Map(document.getElementById('storeMap'),
-        mapOptions);
-}
-google.maps.event.addDomListener(window, 'load', initializeMaps);
+function Location () {
+    var salonLatLng = new google.maps.LatLng(39.946707,-75.302254);
+    var directionService = new google.maps.DirectionsService();
+    var directionsToSalon = new google.maps.DirectionsRenderer();
 
+    this.initializeMaps = function () {
+        var mapOptions = {
+            center: salonLatLng,
+            zoom: 17
+        };
+
+        var map = new google.maps.Map(document.getElementById('storeMap'),
+            mapOptions);
+
+        var marker = new google.maps.Marker({
+            position: salonLatLng,
+            map: map,
+            title: "New Appearances"
+        });
+        directionsToSalon.setMap(map);
+        directionsToSalon.setPanel(document.getElementById('directions'));
+    };
+
+    this.calculateRoute = function () {
+        var request = {
+            origin: "1514 Green St, Philadelphia, PA 19130",
+            destination: salonLatLng,
+            travelMode: google.maps.TravelMode.DRIVING
+        };
+        directionService.route(request, function (response, status) {
+            if (status == google.maps.DirectionsStatus.OK) {
+                directionsToSalon.setDirections(response);
+            }
+        });
+    };
+}
+
+$(document).ready(function (){
+    var local = new Location();
+    google.maps.event.addDomListener(window, 'load', local.initializeMaps);
+    $("#routeCalculate").click(local.calculateRoute);
+});
 
 
