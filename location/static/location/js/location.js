d6a5149ec24228a5e029f5b69268def2efa06a97
PRS <Phil.R.Simmons@gmail.com>
2015-04-13 19:07:22 -0400

    Added better formatting for locations app, added functionality for directions lookup

--- location/static/location/js/location.js
+++ location/static/location/js/location.js
@@ -3,15 +3,25 @@
  * @module Location
  */
 
+/**
+ * Class for dealing with Location data
+ *
+ * @constructor
+ * @class Location
+ */
 function Location () {
     var salonLatLng = new google.maps.LatLng(39.946707,-75.302254);
     var directionService = new google.maps.DirectionsService();
     var directionsToSalon = new google.maps.DirectionsRenderer();
 
+    /**
+     * Create a Google Map for the page
+     * @method initializeMaps
+     */
     this.initializeMaps = function () {
         var mapOptions = {
             center: salonLatLng,
-            zoom: 17
+            zoom: 15
         };
 
         var map = new google.maps.Map(document.getElementById('storeMap'),
@@ -26,9 +36,13 @@ function Location () {
         directionsToSalon.setPanel(document.getElementById('directions'));
     };
 
-    this.calculateRoute = function () {
+    /**
+     * Find the route between the entered location and the salon and display it on the page
+     * @param {String} originAddress
+     */
+    this.calculateRoute = function (originAddress) {
         var request = {
-            origin: "1514 Green St, Philadelphia, PA 19130",
+            origin: originAddress,
             destination: salonLatLng,
             travelMode: google.maps.TravelMode.DRIVING
         };
@@ -38,12 +52,35 @@ function Location () {
             }
         });
     };
+
+    /**
+     * Parse the directions form for address data and cast it to a formatted string
+     * @returns {String} The address which has been entered by the user
+     */
+    this.processForm = function (){
+        var address = "";
+        $.each($("#directionsForm").serializeArray() , function(index, formSet){
+            var value = formSet['value'];
+            if(value){
+                if($.inArray(formSet['name'], ["Zip", "State"])==-1){
+                    address += value + ", ";
+                }
+                else {
+                    address += value + " ";
+                }
+            }
+        });
+        return address;
+    };
 }
 
 $(document).ready(function (){
     var local = new Location();
     google.maps.event.addDomListener(window, 'load', local.initializeMaps);
-    $("#routeCalculate").click(local.calculateRoute);
+    $("")
+    $("#attemptCalculation").click(function (){
+        local.calculateRoute(local.processForm());
+    });
 });
 
 
