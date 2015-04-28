659544aacbe8f4009bc07fa244949da881c20d8a
prs <Phil.R.Simmons@gmail.com>
2015-04-28 01:51:22 -0400

    Got a bunch of the outsstanding formatting/documentation/idiomatic issues out of the Services app

--- services/tests.py
+++ services/tests.py
@@ -2,19 +2,35 @@ from django.test import TestCase
 
 from services.models import Service
 
+
 class TestPriceStr(TestCase):
+    """
+    Test suite for Price string functionality
+    """
     def testRangeString(self):
-        service = Service(minimum_cost = 30, maximum_cost=40)
+        """
+        Tests the ranged display case
+        """
+        service = Service(minimum_cost=30, maximum_cost=40)
         self.assertEquals(service.priceStr(), "$30 - $40")
 
     def testMaxString(self):
+        """
+        Checks the single value display case
+        """
         service = Service(maximum_cost=40)
         self.assertEquals(service.priceStr(), "$40")
 
     def testMinString(self):
+        """
+        Checks the & up display case
+        """
         service = Service(minimum_cost = 30)
         self.assertEquals(service.priceStr(), "$30 & up")
 
-    def testMinString(self):
+    def testNoString(self):
+        """
+        Checks the case of no prices set
+        """
         service = Service()
         self.assertIsNone(service.priceStr())
