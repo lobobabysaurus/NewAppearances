7c7783da13820479310d53f2dbe5961bc72735b8
prs <Phil.R.Simmons@gmail.com>
2015-05-15 10:21:40 -0400

    Changed order to be a composite key with the category type

--- services/models.py
+++ services/models.py
@@ -10,7 +10,7 @@ class Service(models.Model):
             help_text="Minimum cost of a range price service or base price of an & up services",)
     maximum_cost = models.IntegerField(blank=True, null=True,
             help_text="Maximum cost of a range price service or price of a fixed price service",)
-    order = models.IntegerField(unique=True, help_text="Value to order what services should appear where")
+    order = models.IntegerField(help_text="Value to order what services should appear where")
 
     # Constants to represent service types
     HAIRCUTS = 1
@@ -29,7 +29,13 @@ class Service(models.Model):
             help_text="Mapping of constants to strings of service types so that we can save database space",)
     # Reverse key/value lookup
     category_reverse = {category: cat_int for cat_int, category in dict(category_choices).items()}
-    
+
+    class Meta:
+        """
+        Defines composite unique key of category and order
+        """
+        unique_together = ('category', 'order',)
+
     def price_str(self):
         """
         Formats a string representing the price depending on minimum and maximum costs
