6e5896ed9dbbb462fbda7ebfa4eeb2565c3b00db
prs <Phil.R.Simmons@gmail.com>
2015-05-23 18:01:10 -0400

    Made services test module have better documentation

--- services/tests.py
+++ services/tests.py
@@ -12,25 +12,25 @@ class TestPriceStr(TestCase):
         Tests the ranged display case
         """
         service = Service(minimum_cost=30, maximum_cost=40)
-        self.assertEquals(service.price_str(), "$30 - $40")
+        self.assertEquals(service.price_str(), "$30 - $40", "Price range display not as expected")
 
     def testMaxString(self):
         """
         Checks the single value display case
         """
         service = Service(maximum_cost=40)
-        self.assertEquals(service.price_str(), "$40")
+        self.assertEquals(service.price_str(), "$40", "Single value price display not as expected")
 
     def testMinString(self):
         """
         Checks the & up display case
         """
         service = Service(minimum_cost=30)
-        self.assertEquals(service.price_str(), "$30 & up")
+        self.assertEquals(service.price_str(), "$30 & up", "And Up display not as expected")
 
     def testNoString(self):
         """
         Checks the case of no prices set
         """
         service = Service()
-        self.assertIsNone(service.price_str())
+        self.assertIsNone(service.price_str(), "No price display not as expected")
