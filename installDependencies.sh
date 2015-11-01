7b7ad3047cdb773318b099eaca7667c63780e55c
prs <Phil.R.Simmons@gmail.com>
2015-11-01 17:03:56 -0500

    Modified setup script and requirements to use local node modules and python virtual environment instead of global installations

--- installDependencies.sh
+++ installDependencies.sh
@@ -2,5 +2,5 @@
 
 sudo apt-get install python3-pip python-dev libmysqlclient-dev libjpeg-dev python-epydoc  python3-docutils \
                      npm mysql-server mysql-workbench git openjdk-7-jre
-sudo pip3 install -r requirements.txt
-sudo npm install -g http-server yuidocjs less coffee-script
+pip3 install -r requirements.txt
+npm install
